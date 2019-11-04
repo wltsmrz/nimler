1633b3f17e3853f0928c9141936639f9e88a1e98
wltsmrz <wltsmrz@gmail.com>
2019-11-04 02:49:44 +0000

    Update resource example

--- example/resource/Controller.exs
+++ example/resource/Controller.exs
@@ -2,13 +2,15 @@
 defmodule PIController do
     @on_load :load_nif
 
-    def create_resource(_a, _b, _c, _d, _e, _f), do: raise "not implemented"
+    def create_resource(), do: raise "not implemented"
     def update_resource(_a, _b, _c), do: raise "not implemented"
 
     def load_nif do
-        IO.inspect(:erlang.load_nif(to_charlist(Path.join(Path.dirname(__ENV__.file), "nif")), 0))
-        ctrl = create_resource(100.0, -10.0, 10.0, 0.5, 0.25, 1.0)
+        :erlang.load_nif(to_charlist(Path.join(Path.dirname(__ENV__.file), "nif")), 0)
+
+        ctrl = create_resource()
         IO.inspect(update_resource(ctrl, 10.0, 1.0), label: 'PIControl update')
+        # :erlang.garbage_collect()
         IO.inspect(update_resource(ctrl, 10.0, 5.0), label: 'PIControl update')
         IO.inspect(update_resource(ctrl, 10.0, 8.0), label: 'PIControl update')
     end
--- example/resource/controller.nim
+++ example/resource/controller.nim
@@ -12,11 +12,18 @@ type PIControl* = object
   max: float
   integrate: bool
 
-proc init_controller*(control: ptr PIControl, rate: float, min: float, max: float) =
+proc init_controller*(control: ptr PIControl, rate: float, min: float, max: float, kp: float = 0.0, ki: float = 0.0, kf: float = 0.0) =
   control.min = min
   control.max = max
   control.di = 1.0 / rate
   control.integrate = true
+  control.kp = kp
+  control.ki = ki
+  control.kf = kf
+  control.p = 0.0
+  control.i = 0.0
+  control.f = 0.0
+  control.r = 0.0
 
 proc set_gain*(control: ptr PIControl, kp: float, ki: float, kf: float) =
   control.kp = kp

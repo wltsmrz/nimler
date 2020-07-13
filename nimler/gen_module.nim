3dd749ab9685f903310afa98e927003f2c908e21
wltsmrz <wlt@smrz.dev>
2020-07-13 04:56:12 +0000

    Cleanup

--- nimler/gen_module.nim
+++ nimler/gen_module.nim
@@ -35,6 +35,7 @@ proc gen_wrapper*(module_name: string, funcs: static openArray[ErlNifFunc]) {.co
       
     module_contents.add("end\n")
 
-    echo "Generating wrapper module: " & module_filepath
+    echo("Generating wrapper module: " & module_filepath)
+
     writeFile(module_filepath, module_contents)
 
